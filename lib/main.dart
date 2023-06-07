8d350523cb1e3b539087648e6ade1b8f952f7ecf
saumyasingh <saumya.singh@nagarro.com>
2023-06-07 18:05:07 +0530

    tried implementing notifications was unscuccesful

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'views/home_screen.dart';
 import 'package:web_project/views/profile_screen.dart';
 
-void main() {
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   runApp(const MyApp());
 }
 
